8d1a304bdf3faa1dd5619220e2f6794b0ad418ef
Davi Silva <davi.cunha.silva.ca@gmail.com>
2021-06-14 09:24:38 -0300

    feat(test): test
    test

--- src/routes/chat/index.ts
+++ src/routes/chat/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
-import { index, createChat } from '@controllers/chat';
+import {
+  // index,
+  createChat,
+} from '@controllers/chat';
 import { authenticateUser } from '@middlewares/authorization';
 
 const router = Router();
 
-router.get('/index', index);
+// router.get('/index', index);
 
 router.post('/create', authenticateUser, createChat);
 
